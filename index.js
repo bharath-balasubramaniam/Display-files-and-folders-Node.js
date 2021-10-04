a7c2ee3db7175d96d332afc088f4ad799c670f16
Bharath Balasubramaniam <79153733+bharath-balasubramaniam@users.noreply.github.com>
2021-10-05 00:47:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  //   fs.readdir("C:Program Files", (err, data) => {
+  //     if (err) console.log(`Error: ${err.message}`);
+  //     res.send(
+  //       data
+  //         .map((obj) => {
+  //           return obj.includes(".")
+  //             ? `<div style={"text-align:center"}>💾${obj}</div>`
+  //             : `<div style={"text-align:center"}>📁${obj}</div>`;
+  //         })
+  //         .join(" ")
+  //     );
+  //   });
+  fs.readdir("C:\\program Files", (err, data) => {
+    if (err) {
+      console.log(`Error: ${err.message}`);
+    }
+
+    console.log(data);
+    res.send(
+      data
+        .map((f) => {
+          return f.includes(".")
+            ? `<div style="color:grey;font-size:15px">💾${f}</div>`
+            : `<div style="color:black;font-size:15px">📁${f}</div>`;
+        })
+        .join(" ")
+    );
+  });
+});
+app.listen(5000, "127.0.0.1", (err) =>
+  console.log("The server is looking for request")
+);
